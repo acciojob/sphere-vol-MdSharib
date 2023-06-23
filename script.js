97c05a33ce6ff8570480fc8172120f6815d04d61
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-23 12:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function volume_sphere(ev) {
    ev.preventDefault();
   const r = document.getElementById("radius").value;
   const pi = 3.14;
-  const vol = (4 * pi * r * r *r) / 3;
+  let vol = (4 * pi * r * r *r) / 3;
   let res = document.getElementById("volume");
-  res.value = Number((Math.floor(vol * 100) / 100).toFixed(2));
+  res.value = vol.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
